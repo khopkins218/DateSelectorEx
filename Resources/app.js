43a692487e07ce619170b78721b9a41f81937c9b
Kevin Hopkins <kevin@wearefound.com>
2011-03-21 20:15:01 -0400

    removed some unneccessary stuff for this example

--- Resources/app.js
+++ Resources/app.js
@@ -29,36 +29,9 @@ var label1 = Titanium.UI.createLabel({
 win1.add(label1);
 
 //
-// create controls tab and root window
-//
-var win2 = Titanium.UI.createWindow({  
-    title:'Tab 2',
-    backgroundColor:'#fff'
-});
-var tab2 = Titanium.UI.createTab({  
-    icon:'KS_nav_ui.png',
-    title:'Tab 2',
-    window:win2
-});
-
-var label2 = Titanium.UI.createLabel({
-	color:'#999',
-	text:'I am Window 2',
-	font:{fontSize:20,fontFamily:'Helvetica Neue'},
-	textAlign:'center',
-	width:'auto'
-});
-
-win2.add(label2);
-
-
-
-//
 //  add tabs
 //
 tabGroup.addTab(tab1);  
-tabGroup.addTab(tab2);  
-
 
 // open tab group
 tabGroup.open();
